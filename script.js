f75000da74360031fbabb0543ef381760bec5c85
Rakibul-hassan-shehab <mdshehabsaddir486@gmail.com>
2023-06-22 10:18:48 +0600

    first commit

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const colors = ['red','yellow','green', 'blue','orange','purple','violet','indigo','black','white'];
+let slide = document.querySelectorAll('.slide');
+let pre = document.querySelector('#pev');
+let nxt = document.querySelector('#nxt');
+var count = 0;
+slide.forEach((slide,index)=>{
+    slide.style.left =`${index * 100}%`
+})
 
-let ctn = document.querySelector('section');
-let h1 = document.querySelector('h1');
-let span = document.querySelector('#spanColor');
+function slideImg(){
+    slide.forEach((slide)=>{
+        slide.style.transform = `translateX(-${count * 100}%)`
+    })
+}
+pre.addEventListener('click', function(){
+    count--;
+    if(count < 0){
+        count = slide.length - 1;
+        slideImg()
+    }else{
+        slideImg()
+    }
+})
 
-for( let color of colors){
-    let box = document.createElement('div');
-    box.classList.add('box');
-
-    ctn.append(box);
-    box.style.backgroundColor = color;
-    
-    box.addEventListener('mouseover', function(){
-        h1.innerText = 'You Are Selected :';
-        span.innerText = box.style.backgroundColor;
-        span.style.backgroundColor = box.style.backgroundColor;
-    });
-};
+nxt.addEventListener('click', function(){
+    count++;
+    if(count >= slide.length){
+        count = 0;
+        slideImg()
+    }else{
+        slideImg()
+    }
+})
